6a8fa9f8f25694f8ffcb4ddf5fc25f1608a34ad6
Hunter Carlson <carlson.hunter@gmail.com>
2020-12-04 14:01:16 -0700

    Animate hue shift with ffmpeg

--- MandelbrotNetCore/Program.cs
+++ MandelbrotNetCore/Program.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
 
 namespace MandelbrotNetCore
 {
     internal class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
             Console.WriteLine("Hello Mandelbrot!");
 
             //ParallelVsSerial();
 
-            Draw();
+            var renderer = new Renderer(1000);
+            Bitmap bmp = renderer.Draw();
+            bmp.Save("MandelbrotRender.png", ImageFormat.Png);
+
+            //RenderFrames(400);
+
+            //await RenderVideo(400);
 
             Console.WriteLine("Done");
 
@@ -65,44 +76,6 @@ namespace MandelbrotNetCore
             Console.WriteLine();
         }
 
-        private static void Draw()
-        {
-            var mandelbrot = new Mandelbrot();
-
-            const int pxSize = 1000;
-            const int maxIterations = 100;
-
-            var bitmap = new Bitmap(pxSize, pxSize);
-
-            int[][] iterations = mandelbrot.Generate(pxSize, maxIterations);
-
-            double hueStep = 36;
-            double hueOffset = 0;
-
-            for (int i = 0; i < pxSize; i++)
-            {
-                for (int j = 0; j < pxSize; j++)
-                {
-                    Color color;
-
-                    if (iterations[i][j] == maxIterations || iterations[i][j] == 0)
-                    {
-                        color = Color.Black;
-                    }
-                    else
-                    {
-                        double hue = (hueStep * (iterations[i][j] - 1) + hueOffset) % 360;
-                        ColorConverter.HsvToRgb(hue, 1, 1, out int r, out int g, out int b);
-                        color = Color.FromArgb(255, r, g, b);
-                    }
-
-                    bitmap.SetPixel(i, j, color);
-                }
-            }
-
-            bitmap.Save("MandelbrotRender.png", ImageFormat.Png);
-        }
-
         private static void GenTestData()
         {
             var mandelbrot = new Mandelbrot();
@@ -111,6 +84,63 @@ namespace MandelbrotNetCore
 
             (List<Complex> iterationSteps, List<double> magnitude) iterationSteps =
                 Mandelbrot.IterateZWithLogging(new Complex(-0.38, 0.44), 100);
+        }
+
+        /// <summary>
+        ///     Render hue shift frames for animation
+        /// </summary>
+        private static void RenderFrames(int pxSize)
+        {
+            // Make a folder for the frames
+            if (!Directory.Exists("Frames"))
+            {
+                Directory.CreateDirectory("Frames");
+            }
+
+            // Make a folder for the pxSize
+            if (!Directory.Exists($"Frames/{pxSize}px"))
+            {
+                Directory.CreateDirectory($"Frames/{pxSize}px");
+            }
+
+            // Render
+            var renderer = new Renderer(400);
+
+            for (int i = 0; i < 360; i++)
+            {
+                Bitmap bmp = renderer.Draw(hueOffset: i);
+                // add leading 0s to file name
+                string frameNum = $"{i}".PadLeft(3, '0');
+                bmp.Save($"Frames/{pxSize}px/Frame_{frameNum}.png", ImageFormat.Png);
+
+                if (i % 10 == 0)
+                {
+                    int percentComplete = (int) (i / 360.0 * 100);
+                    Console.WriteLine($"Rendered frame {i} of 360 ({percentComplete}%)");
+                }
+            }
+        }
+
+        private static async Task RenderVideo(int pxSize)
+        {
+            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official);
+
+            string dir = $"Frames/{pxSize}px";
+
+            if (!Directory.Exists(dir))
+            {
+                throw new DirectoryNotFoundException(dir);
+            }
+
+            List<string> files = Directory.EnumerateFiles(dir).ToList();
+
+            await FFmpeg.Conversions.New()
+                .BuildVideoFromImages(files)
+                .SetFrameRate(60)
+                .SetOutput($"Mandelbrot{pxSize}px.mp4")
+                .Start();
+
+            Console.WriteLine("Video Rendered!");
         }
     }
 }
---
+++ MandelbrotNetCore/Renderer.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+
+namespace MandelbrotNetCore
+{
+    public class Renderer
+    {
+        private readonly int _pxSize;
+        private readonly int _maxIterations;
+
+        private readonly int[][] _iterations;
+
+        public Renderer(int pxSize, int maxIterations = 100)
+        {
+            _pxSize = pxSize;
+            _maxIterations = maxIterations;
+
+            var mandelbrot = new Mandelbrot();
+            _iterations = mandelbrot.GenerateParallel(_pxSize, _maxIterations);
+        }
+
+        public Bitmap Draw(double hueStep = 36.0, double hueOffset = 0.0)
+        {
+            var bitmap = new Bitmap(_pxSize, _pxSize);
+
+            //var stopwatch = new Stopwatch();
+            //stopwatch.Start();
+
+            for (int i = 0; i < _pxSize; i++)
+            {
+                for (int j = 0; j < _pxSize; j++)
+                {
+                    Color color;
+
+                    if (_iterations[i][j] == _maxIterations || _iterations[i][j] == 0)
+                    {
+                        color = Color.Black;
+                    }
+                    else
+                    {
+                        double hue = (hueStep * (_iterations[i][j] - 1) + hueOffset) % 360;
+                        ColorConverter.HsvToRgb(hue, 1, 1, out int r, out int g, out int b);
+                        color = Color.FromArgb(255, r, g, b);
+                    }
+
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.Elapsed);
+
+            return bitmap;
+        }
+    }
+}
